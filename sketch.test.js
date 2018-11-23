0561a7cc7a355f7fb49d484f8ceb1e12b90b4752
Brad <zellefrow@Brads-MacBook-Pro.local>
2018-11-22 21:53:59 -0700

    changed import

--- sketch.test.js
+++ sketch.test.js
@@ -4,23 +4,23 @@ import {sum,sub,mult,div} from './sketch';
 
 
 test('adds two values',()=>{
-	expect(sketch.sum(1,2)).toBe(3);
+	expect(sum(1,2)).toBe(3);
 })
 
 test('subtracts two values neg',()=>{
-	expect(sketch.sub(2,1)).toBe(1);
+	expect(sub(2,1)).toBe(1);
 })
 
 test('subtracts two values pos',()=>{
-	expect(sketch.sub(1,2)).toBe(-1);
+	expect(sub(1,2)).toBe(-1);
 })
 
 test ('multiply two values',()=>{
-	expect(sketch.sub(3,2)).toBe(6);
+	expect(sub(3,2)).toBe(6);
 })
 
 
 test('divide two values',()=>{
-	expect(sketch.div(4,2)).toBe(2);
+	expect(div(4,2)).toBe(2);
 })
 
