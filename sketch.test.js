af004d98fb4651fdb51c36f3a869887a0b6c7ced
Brad <zellefrow@Brads-MacBook-Pro.local>
2018-11-22 21:49:26 -0700

    changed exports

--- sketch.test.js
+++ sketch.test.js
@@ -1,23 +1,25 @@
-const sum = require('./sketch');
+//const sum = require('./sketch');
+import sketch from './sketch'
+
 
 test('adds two values',()=>{
-	expect(sum(1,2)).toBe(3);
+	expect(sketch.sum(1,2)).toBe(3);
 })
 
 test('subtracts two values neg',()=>{
-	expect(sub(2,1)).toBe(1);
+	expect(sketch.sub(2,1)).toBe(1);
 })
 
 test('subtracts two values pos',()=>{
-	expect(sub(1,2)).toBe(-1);
+	expect(sketch.sub(1,2)).toBe(-1);
 })
 
 test ('multiply two values',()=>{
-	expect(sub(3,2)).toBe(6);
+	expect(sketch.sub(3,2)).toBe(6);
 })
 
 
 test('divide two values',()=>{
-	expect(div(4,2)).toBe(2);
+	expect(sketch.div(4,2)).toBe(2);
 })
 
